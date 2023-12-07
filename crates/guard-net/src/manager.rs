bb6d43c9d14efdcc916377fc94ae93e254c05f18
Will Smith <williamqsmith11@gmail.com>
2023-12-07 00:10:03 -0500

    wip

--- crates/guard-net/src/manager.rs
+++ crates/guard-net/src/manager.rs
@@ -6,13 +6,15 @@ use std::{
 };
 
 use futures::{task::Poll, StreamExt};
+use guard_types::consensus::{Commit, PreProposal, Proposal};
 use reth_eth_wire::DisconnectReason;
 use reth_metrics::common::mpsc::UnboundedMeteredSender;
 use reth_primitives::PeerId;
+use tokio::sync::mpsc::UnboundedSender;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tracing::error;
 
-use crate::{NetworkOrderEvent, StromNetworkHandleMsg, Swarm};
+use crate::{NetworkOrderEvent, StromMessage, StromNetworkHandleMsg, Swarm, SwarmEvent};
 #[allow(unused_imports)]
 use crate::{StromNetworkConfig, StromNetworkHandle, StromSessionManager};
 
@@ -20,10 +22,11 @@ use crate::{StromNetworkConfig, StromNetworkHandle, StromSessionManager};
 pub struct StromNetworkManager {
     handle:               StromNetworkHandle,
     from_handle_rx:       UnboundedReceiverStream<StromNetworkHandleMsg>,
-    swarm:                Swarm,
     to_pool_manager:      Option<UnboundedMeteredSender<NetworkOrderEvent>>,
-    to_consensus_manager: Option<UnboundedMeteredSender<StromNetworkEvent>>,
+    to_consensus_manager: Option<UnboundedMeteredSender<StromConsensusEvent>>,
+    subscriptions:        Vec<UnboundedSender<UnboundedSender<StromNetworkEvent>>>,
 
+    swarm:            Swarm,
     /// This is updated via internal events and shared via `Arc` with the
     /// [`NetworkHandle`] Updated by the `NetworkWorker` and loaded by the
     /// `NetworkService`.
@@ -50,8 +53,12 @@ impl StromNetworkManager {
             StromNetworkHandleMsg::RemovePeer(peer_id) => {
                 self.swarm.state_mut().remove_peer(peer_id);
             }
-
-            _ => todo!()
+            StromNetworkHandleMsg::ReputationChange(peer_id, kind) => {
+                self.swarm.state_mut().change_weight(peer_id, kind)
+            }
+            _ => {
+                todo!()
+            }
         }
     }
 
@@ -66,12 +73,10 @@ impl StromNetworkManager {
 impl Future for StromNetworkManager {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let this = self.get_mut();
-
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         // process incoming messages from a handle
         loop {
-            match this.from_handle_rx.poll_next_unpin(cx) {
+            match self.from_handle_rx.poll_next_unpin(cx) {
                 Poll::Pending => break,
                 Poll::Ready(None) => {
                     // This is only possible if the channel was deliberately closed since we always
@@ -79,8 +84,48 @@ impl Future for StromNetworkManager {
                     error!("Strom network message channel closed.");
                     return Poll::Ready(())
                 }
-                Poll::Ready(Some(msg)) => this.on_handle_message(msg)
+                Poll::Ready(Some(msg)) => self.on_handle_message(msg)
             };
+
+            macro_rules! send_msgs {
+                ($name:ident, $peer_id:ident, $($var:ident),+) => {
+                    match $name {
+                        $(
+                            StromMessage::$var(a) => {
+                                self.to_consensus_manager
+                                    .as_mut()
+                                    .map(|tx| tx.send(StromConsensusEvent::$var($peer_id, a)));
+                            },
+                        )+
+                        StromMessage::PropagatePooledOrders(a) => {
+                            self.to_pool_manager
+                                .as_mut()
+                                .map(|tx| tx.send(NetworkOrderEvent::IncomingOrders {
+                                    $peer_id,
+                                    orders: a
+                                }));
+                        },
+                        _ => {}
+                    }
+                };
+            }
+
+            if let Poll::Ready(Some(event)) = self.swarm.poll_next_unpin(cx) {
+                match event {
+                    SwarmEvent::ValidMessage { peer_id, msg } => {
+                        send_msgs!(msg, peer_id, Commit, Propose, PrePropose)
+                    }
+                    // StromMessage::Commit(c) => self
+                    //     .to_consensus_manager
+                    //     .as_mut()
+                    //     .map(|tx| tx.send(StromConsensusEvent::Commit(peer_id, c))),
+                    // StromMessage::Propose(p) => self
+                    //     .to_consensus_manager
+                    //     .as_mut()
+                    //     .map(|tx| tx.send(StromConsensusEvent::Commit(peer_id, c)))
+                    SwarmEvent::Disconnected { peer_id } => {}
+                }
+            }
         }
 
         Poll::Pending
@@ -112,4 +157,11 @@ pub enum StromNetworkEvent {
     PeerAdded(PeerId),
     /// Event emitted when a new peer is removed
     PeerRemoved(PeerId)
+}
+
+#[derive(Debug, Clone)]
+pub enum StromConsensusEvent {
+    PrePropose(PeerId, PreProposal),
+    Propose(PeerId, Proposal),
+    Commit(PeerId, Commit)
 }

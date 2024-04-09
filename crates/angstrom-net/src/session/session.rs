d0f9c69d2cbdc70f06dfca123c944009de025aac
Will Smith <williamqsmith11@gmail.com>
2024-04-08 22:13:00 -0400

    w

--- crates/angstrom-net/src/session/session.rs
+++ crates/angstrom-net/src/session/session.rs
@@ -121,7 +121,7 @@ impl StromSession {
                 let handle = self.pending_handle.take().unwrap();
                 let _ = self
                     .to_session_manager
-                    .send_item(StromSessionMessage::Established { handle });
+                    .send_item(StromSessionMessage::Established { handle }).unwrap();
                 tracing::debug!("sent session handle");
                 return None
             }

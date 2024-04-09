5c01d5543b7c6a38e424d7bbd6215346ba4316d7
Will Smith <williamqsmith11@gmail.com>
2024-04-08 21:27:46 -0400

    wip

--- crates/angstrom-net/src/session/session.rs
+++ crates/angstrom-net/src/session/session.rs
@@ -257,7 +257,7 @@ impl StromSession {
         let status = status.verify();
         tracing::debug!(?status, ?self.remote_peer_id);
         let verification =
-            current_time >= status_time && status.verify() == Ok(self.remote_peer_id);
+            current_time >= status_time && status == Ok(self.remote_peer_id);
         tracing::debug!(%verification, "incomming connection results");
         verification
     }

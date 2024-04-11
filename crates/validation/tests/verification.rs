2257ca6219d45ac497c513927091c6ff04e3952a
Will Smith <williamqsmith11@gmail.com>
2024-04-11 19:39:11 -0400

    yer

--- crates/validation/tests/verification.rs
+++ crates/validation/tests/verification.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, path::Path, time::Duration};
+use std::{collections::HashMap, path::Path, pin::pin, time::Duration};
 
 use alloy_primitives::{hex, Address, U256};
+use alloy_sol_types::SolStruct;
 use angstrom_types::orders::{OrderLocation, OrderOrigin, OrderValidationOutcome};
 use futures::future::{select, Either};
 use testing_tools::{
@@ -268,6 +269,185 @@ async fn test_validation_balance_failure() {
         Either::Left((i, _)) => {
             if let OrderValidationOutcome::Valid { order, .. } = i {
                 assert!(order.location == OrderLocation::LimitParked, "missing balance");
+            } else {
+                panic!("order should be valid");
+            }
+        }
+        Either::Right(..) => {
+            panic!("timeout hit on validation");
+        }
+    }
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[serial_test::serial]
+async fn test_validation_balance_runout() {
+    let mut validator = init_tools!();
+
+    // setup order to validate
+    let mut order = generate_rand_valid_limit_order();
+    order.order.currencyIn = WETH_ADDRESS;
+    order.order.currencyOut = USDT_ADDRESS;
+
+    let address = order.recover_signer().unwrap();
+    // overwrite the slots to ensure the balance needed exists
+    let weth_approval = validator
+        .config
+        .approvals
+        .iter()
+        .find(|a| a.token == WETH_ADDRESS)
+        .unwrap();
+
+    let approval_slot = weth_approval
+        .generate_slot(address, ANGSTROM_CONTRACT)
+        .unwrap();
+
+    let weth_balance = validator
+        .config
+        .balances
+        .iter()
+        .find(|a| a.token == WETH_ADDRESS)
+        .unwrap();
+
+    let balance_slot = weth_balance.generate_slot(address).unwrap();
+    let mut state_overrides = HashMap::new();
+
+    let mut weth = HashMap::new();
+    weth.insert(balance_slot, U256::from(order.order.amountIn));
+    weth.insert(approval_slot, U256::MAX);
+
+    state_overrides.insert(WETH_ADDRESS, weth);
+    validator.revm_lru.set_state_overrides(state_overrides);
+
+    let client = validator.client.clone();
+    let client2 = validator.client.clone();
+
+    let mut validator = pin!(validator);
+
+    {
+        let out = select(
+            client.validate_order(OrderOrigin::External, order.clone().try_into().unwrap()),
+            Box::pin(validator.poll_for(Duration::from_millis(100)))
+        )
+        .await;
+
+        match out {
+            Either::Left((i, f)) => {
+                if let OrderValidationOutcome::Valid { order, .. } = i {
+                    assert!(order.location == OrderLocation::LimitPending, "wrong");
+                } else {
+                    panic!("order should be valid");
+                }
+                f.await;
+            }
+            Either::Right((..)) => {
+                panic!("timeout hit on validation");
+            }
+        }
+    }
+
+    // send same order but different nonce
+    order.order.nonce += U256::from(1);
+    order.hash = order.order.eip712_hash_struct();
+
+    let out = select(
+        client2.validate_order(OrderOrigin::External, order.clone().try_into().unwrap()),
+        Box::pin(validator.poll_for(Duration::from_millis(100)))
+    )
+    .await;
+
+    match out {
+        Either::Left((i, _)) => {
+            if let OrderValidationOutcome::Valid { order, .. } = i {
+                assert!(order.location == OrderLocation::LimitParked, "should be parked");
+            } else {
+                panic!("order should be valid");
+            }
+        }
+        Either::Right(..) => {
+            panic!("timeout hit on validation");
+        }
+    }
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[serial_test::serial]
+async fn test_validation_duplicated_nonce() {
+    let mut validator = init_tools!();
+
+    // setup order to validate
+    let mut order = generate_rand_valid_limit_order();
+    order.order.currencyIn = WETH_ADDRESS;
+    order.order.currencyOut = USDT_ADDRESS;
+
+    let address = order.recover_signer().unwrap();
+    // overwrite the slots to ensure the balance needed exists
+    let weth_approval = validator
+        .config
+        .approvals
+        .iter()
+        .find(|a| a.token == WETH_ADDRESS)
+        .unwrap();
+
+    let approval_slot = weth_approval
+        .generate_slot(address, ANGSTROM_CONTRACT)
+        .unwrap();
+
+    let weth_balance = validator
+        .config
+        .balances
+        .iter()
+        .find(|a| a.token == WETH_ADDRESS)
+        .unwrap();
+
+    let balance_slot = weth_balance.generate_slot(address).unwrap();
+    let mut state_overrides = HashMap::new();
+
+    let mut weth = HashMap::new();
+    weth.insert(balance_slot, U256::MAX);
+    weth.insert(approval_slot, U256::MAX);
+
+    state_overrides.insert(WETH_ADDRESS, weth);
+    validator.revm_lru.set_state_overrides(state_overrides);
+
+    let client = validator.client.clone();
+    let client2 = validator.client.clone();
+
+    let mut validator = pin!(validator);
+
+    {
+        let out = select(
+            client.validate_order(OrderOrigin::External, order.clone().try_into().unwrap()),
+            Box::pin(validator.poll_for(Duration::from_millis(100)))
+        )
+        .await;
+
+        match out {
+            Either::Left((i, f)) => {
+                if let OrderValidationOutcome::Valid { order, .. } = i {
+                    assert!(order.location == OrderLocation::LimitPending, "wrong");
+                } else {
+                    panic!("order should be valid");
+                }
+                f.await;
+            }
+            Either::Right((..)) => {
+                panic!("timeout hit on validation");
+            }
+        }
+    }
+
+    // send same order but different nonce
+
+    let out = select(
+        client2.validate_order(OrderOrigin::External, order.clone().try_into().unwrap()),
+        Box::pin(validator.poll_for(Duration::from_millis(100)))
+    )
+    .await;
+
+    match out {
+        Either::Left((i, _)) => {
+            if let OrderValidationOutcome::Invalid(..) = i {
             } else {
                 panic!("order should be valid");
             }

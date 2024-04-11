01910fbc91bbe001331afe41fe39d6b59fc2fa89
Will Smith <williamqsmith11@gmail.com>
2024-04-11 17:19:55 -0400

    wip

--- crates/validation/tests/verification.rs
+++ crates/validation/tests/verification.rs
@@ -1,4 +1,8 @@
+use testing_tools::mocks::eth_events::MockEthEventHandle;
+
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[serial_test::serial]
 async fn test_validation_pass() {
     reth_tracing::init_test_tracing();
+    let (handle, eth_events) = MockEthEventHandle::new();
 }

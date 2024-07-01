cf4cd0580555f0e6d03ff542a4e33f6851d0002f
Will Smith <williamqsmith11@gmail.com>
2024-07-01 19:06:18 -0400

    wip

--- bin/testnet/src/utils/anvil_manager.rs
+++ bin/testnet/src/utils/anvil_manager.rs
@@ -4,10 +4,18 @@ use alloy::{
 };
 use alloy_provider::{builder, RootProvider};
 
-pub async fn spawn_anvil(block_time: u64, fork_url: String) -> eyre::Result<RootProvider<Http<Client>>> {
-    let anvil = Anvil::new().block_time(block_time).fork_block_number(20214717).fork(fork_url).try_spawn()?;
+pub async fn spawn_anvil(
+    block_time: u64,
+    fork_url: String
+) -> eyre::Result<RootProvider<Http<Client>>> {
+    let anvil = Anvil::new()
+        .block_time(block_time)
+        .fork_block_number(20214717)
+        .fork(fork_url)
+        .try_spawn()?;
     let endpoint = anvil.endpoint_url();
     let rpc = builder().on_http(endpoint);
+
     tracing::info!("connected to anvil");
 
     Ok(rpc)

7652976d6ec9f1bdc142b56724b5ebdb2d28149d
Will Smith <williamqsmith11@gmail.com>
2024-07-03 16:09:44 -0400

    quicker startup

--- bin/testnet/src/contract_setup/mod.rs
+++ bin/testnet/src/contract_setup/mod.rs
@@ -1,5 +1,10 @@
+use std::{pin::pin, time::Duration};
+
 use alloy_primitives::{Address, U256};
+use alloy_provider::ext::AnvilApi;
+use futures::Future;
 use sol_bindings::testnet::{MockERC20, PoolManagerDeployer, TestnetHub};
+use tokio::time::timeout;
 
 use crate::anvil_utils::AnvilWalletRpc;
 
@@ -15,13 +20,37 @@ pub struct AngstromTestnetAddresses {
 pub async fn deploy_contract_and_create_pool(
     provider: AnvilWalletRpc
 ) -> eyre::Result<AngstromTestnetAddresses> {
-    let out = PoolManagerDeployer::deploy(provider.clone(), U256::MAX).await?;
+    let out = anvil_mine_delay(
+        Box::pin(PoolManagerDeployer::deploy(provider.clone(), U256::MAX)),
+        &provider,
+        Duration::from_millis(500)
+    )
+    .await?;
+
     let v4_address = *out.address();
-    let testhub = TestnetHub::deploy(provider.clone(), Address::ZERO, v4_address).await?;
+    let testhub = anvil_mine_delay(
+        Box::pin(TestnetHub::deploy(provider.clone(), Address::ZERO, v4_address)),
+        &provider,
+        Duration::from_millis(500)
+    )
+    .await?;
     let angstrom_address = *testhub.address();
 
-    let token0 = MockERC20::deploy(provider.clone()).await?;
-    let token1 = MockERC20::deploy(provider.clone()).await?;
+    // if we don't do these sequentially, the provider nonce messes up and doesn't
+    // deploy properly
+    let token0 = anvil_mine_delay(
+        Box::pin(MockERC20::deploy(provider.clone())),
+        &provider,
+        Duration::from_millis(500)
+    )
+    .await?;
+    let token1 = anvil_mine_delay(
+        Box::pin(MockERC20::deploy(provider.clone())),
+        &provider,
+        Duration::from_millis(500)
+    )
+    .await?;
+
     let token0 = *token0.address();
     let token1 = *token1.address();
 
@@ -34,4 +63,25 @@ pub async fn deploy_contract_and_create_pool(
     );
 
     Ok(AngstromTestnetAddresses { contract: angstrom_address, token0, token1 })
+}
+
+// will wait for a specific delay and then call anvil mine wallet.
+// this will allow us to quick mine and not have to wait the 12 seconds
+// between transactions while avoiding race conditions
+pub async fn anvil_mine_delay<F0: Future + Unpin>(
+    f0: F0,
+    provider: &AnvilWalletRpc,
+    delay: Duration
+) -> F0::Output {
+    let mut pinned = pin!(f0);
+    match timeout(delay, &mut pinned).await {
+        Ok(v) => return v,
+        Err(_) => {}
+    }
+    provider
+        .anvil_mine(Some(U256::from(1)), None)
+        .await
+        .expect("anvil failed to mine");
+
+    pinned.await
 }

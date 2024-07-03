78d215fb71ca01d07ceef5f348523437dbadaf90
Will Smith <williamqsmith11@gmail.com>
2024-07-03 14:42:46 -0400

    fix

--- crates/sol-bindings/src/lib.rs
+++ crates/sol-bindings/src/lib.rs
@@ -1,9 +1,8 @@
-pub mod contract_bundle_ext;
 pub mod envelopes;
+pub mod ext;
 pub mod sol;
 pub mod testnet;
 pub mod user_types;
-pub mod ext;
 pub use ext::*;
 
 pub use crate::sol::AngstromContract;

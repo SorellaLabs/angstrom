65a3d3937b7a557729f875229869f081ad1127ab
Will Smith <williamqsmith11@gmail.com>
2024-07-03 13:31:56 -0400

    wip

--- bin/testnet/src/anvil_utils/anvil_manager.rs
+++ bin/testnet/src/anvil_utils/anvil_manager.rs
@@ -1,6 +1,6 @@
-use alloy::node_bindings::Anvil;
+use alloy::{node_bindings::Anvil, signers::local::PrivateKeySigner};
 use alloy_node_bindings::AnvilInstance;
-use alloy_provider::{builder, IpcConnect, RootProvider};
+use alloy_provider::{builder, IpcConnect, ProviderBuilder, RootProvider};
 use alloy_pubsub::PubSubFrontend;
 
 pub async fn spawn_anvil(
@@ -18,9 +18,17 @@ pub async fn spawn_anvil(
     let endpoint = "/tmp/anvil.ipc";
     tracing::info!(?endpoint);
     let ipc = IpcConnect::new(endpoint.to_string());
-    let rpc = builder().on_ipc(ipc).await?;
+    let sk: PrivateKeySigner = anvil.keys()[0].clone().into();
+
+    let rpc = ProviderBuilder::new()
+        .with_recommended_fillers()
+        .wallet(sk)
+        .on_ipc(ipc)
+        .await
+        .unwrap();
 
     tracing::info!("connected to anvil");
 
-    Ok((anvil, rpc))
+    todo!()
+    // Ok((anvil, rpc))
 }
--- bin/testnet/src/contract_setup/mod.rs
+++ bin/testnet/src/contract_setup/mod.rs
@@ -1,1 +1,35 @@
+use alloy_primitives::Address;
+use alloy_primitives::U256;
+use alloy_provider::Provider;
+use alloy_provider::{ext::AnvilApi, RootProvider};
+use alloy_pubsub::PubSubFrontend;
+use alloy_sol_types::SolCall;
+// use secp256k1::{SECP256K1,SecretKey};
+use contract_bytecodes::POOL_MANAGER;
+use enr::k256::SecretKey;
+// use enr::secp256k1::SecretKey;
+// use enr::k256::SecretKey;
+use sol_bindings::testnet::{PoolManagerDeployer, TestnetHub};
 
+pub mod contract_bytecodes;
+
+pub struct AngstromTestnetAddresses {
+    pub contract: Address,
+    pub token0:   Address,
+    pub token1:   Address
+}
+/// deploys the angstrom testhub contract along with two tokens, under the secret key
+pub async fn deploy_contract_and_create_pool(
+    pk: SecretKey,
+    provider: RootProvider<PubSubFrontend>
+) -> eyre::Result<AngstromTestnetAddresses> {
+    
+    let mut pool_bytecode = POOL_MANAGER.clone().to_vec();
+    PoolManagerDeployer::deploy(provider, U256::MAX);
+    let signer = alloy_
+
+    provider.send_transaction(tx)
+
+
+    todo!()
+}

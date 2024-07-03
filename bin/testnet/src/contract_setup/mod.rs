4de07a2c72b0c4a414f32b3a438a0f3e02be9add
Will Smith <williamqsmith11@gmail.com>
2024-07-03 13:33:23 -0400

    wip

--- bin/testnet/src/contract_setup/mod.rs
+++ bin/testnet/src/contract_setup/mod.rs
@@ -1,7 +1,6 @@
-use alloy_primitives::Address;
-use alloy_primitives::U256;
-use alloy_provider::Provider;
-use alloy_provider::{ext::AnvilApi, RootProvider};
+use alloy::signers::local::PrivateKeySigner;
+use alloy_primitives::{Address, U256};
+use alloy_provider::{ext::AnvilApi, Provider, RootProvider};
 use alloy_pubsub::PubSubFrontend;
 use alloy_sol_types::SolCall;
 // use secp256k1::{SECP256K1,SecretKey};
@@ -18,18 +17,19 @@ pub struct AngstromTestnetAddresses {
     pub token0:   Address,
     pub token1:   Address
 }
-/// deploys the angstrom testhub contract along with two tokens, under the secret key
+/// deploys the angstrom testhub contract along with two tokens, under the
+/// secret key
 pub async fn deploy_contract_and_create_pool(
     pk: SecretKey,
     provider: RootProvider<PubSubFrontend>
 ) -> eyre::Result<AngstromTestnetAddresses> {
-    
-    let mut pool_bytecode = POOL_MANAGER.clone().to_vec();
-    PoolManagerDeployer::deploy(provider, U256::MAX);
-    let signer = alloy_
-
-    provider.send_transaction(tx)
-
+    // let pk:PrivateKeySigner= pk.into();
+    //
+    // let mut pool_bytecode = POOL_MANAGER.clone().to_vec();
+    // let out= PoolManagerDeployer::deploy(provider, U256::MAX).await?;
+    // let signer = alloy_
+    //
+    // provider.send_transaction(tx)
 
     todo!()
 }

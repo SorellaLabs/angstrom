573106c4f7a116338ed5efb85dcf9814987c2825
Will Smith <williamqsmith11@gmail.com>
2024-07-10 18:02:15 -0400

    wip

--- crates/validation/src/order/mod.rs
+++ crates/validation/src/order/mod.rs
@@ -5,7 +5,8 @@ use angstrom_types::{
     rpc::{
         EcRecoveredComposableLimitOrder, EcRecoveredComposableSearcherOrder, EcRecoveredLimitOrder,
         EcRecoveredSearcherOrder
-    }
+    },
+    sol_bindings::grouped_orders::{AllOrders, OrderWithStorageData}
 };
 use tokio::sync::oneshot::{channel, Sender};
 
@@ -18,60 +19,25 @@ pub mod state;
 use crate::validator::ValidationClient;
 
 pub type ValidationFuture<'a, O> =
-    Pin<Box<dyn Future<Output = OrderValidationOutcome<O>> + Send + Sync + 'a>>;
+    Pin<Box<dyn Future<Output = OrderWithStorageData<O>> + Send + Sync + 'a>>;
 
 pub type ValidationsFuture<'a, O> =
-    Pin<Box<dyn Future<Output = Vec<OrderValidationOutcome<O>>> + Send + Sync + 'a>>;
+    Pin<Box<dyn Future<Output = Vec<OrderWithStorageData<O>>> + Send + Sync + 'a>>;
 
 pub enum OrderValidationRequest {
-    ValidateLimit(
-        Sender<OrderValidationOutcome<EcRecoveredLimitOrder>>,
-        OrderOrigin,
-        EcRecoveredLimitOrder
-    ),
-    ValidateSearcher(
-        Sender<OrderValidationOutcome<EcRecoveredSearcherOrder>>,
-        OrderOrigin,
-        EcRecoveredSearcherOrder
-    ),
-    ValidateComposableLimit(
-        Sender<OrderValidationOutcome<EcRecoveredComposableLimitOrder>>,
-        OrderOrigin,
-        EcRecoveredComposableLimitOrder
-    ),
-    ValidateComposableSearcher(
-        Sender<OrderValidationOutcome<EcRecoveredComposableSearcherOrder>>,
-        OrderOrigin,
-        EcRecoveredComposableSearcherOrder
-    )
+    ValidateOrder(Sender<OrderWithStorageData<AllOrders>>, OrderOrigin)
 }
 
 /// Provides support for validating transaction at any given state of the chain
 pub trait OrderValidatorHandle: Send + Sync + Clone + Debug + Unpin + 'static {
     /// The order type of the limit order pool
-    type LimitOrder: PoolOrder;
-
-    /// The transaction type of the searcher order pool
-    type SearcherOrder: PoolOrder;
-
-    /// The transaction type of the composable limit order pool
-    type ComposableLimitOrder: PoolOrder;
-
-    /// The transaction type of the composable searcher order pool
-    type ComposableSearcherOrder: PoolOrder;
-
-    /// Validates the order and returns a [`OrderValidationOutcome`]
-    /// describing the validity of the given order
-    ///
-    /// This will be used by the order-pool to check whether the
-    /// transaction should be inserted into the pool or discarded right
-    /// away.
+    type Order: Send + Sync;
 
     fn validate_order(
         &self,
         origin: OrderOrigin,
-        transaction: Self::LimitOrder
-    ) -> ValidationFuture<Self::LimitOrder>;
+        transaction: Self::Order
+    ) -> ValidationFuture<Self::Order>;
 
     /// Validates a batch of orders.
     ///
@@ -79,140 +45,33 @@ pub trait OrderValidatorHandle: Send + Sync + Clone + Debug + Unpin + 'static {
 
     fn validate_orders(
         &self,
-        transactions: Vec<(OrderOrigin, Self::LimitOrder)>
-    ) -> ValidationsFuture<Self::LimitOrder> {
+        transactions: Vec<(OrderOrigin, Self::Order)>
+    ) -> ValidationsFuture<Self::Order> {
         Box::pin(futures_util::future::join_all(
             transactions
                 .into_iter()
                 .map(|(origin, tx)| self.validate_order(origin, tx))
         ))
     }
-
-    fn validate_composable_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::ComposableLimitOrder
-    ) -> ValidationFuture<Self::ComposableLimitOrder>;
-
-    fn validate_composable_orders(
-        &self,
-        transactions: Vec<(OrderOrigin, Self::ComposableLimitOrder)>
-    ) -> ValidationsFuture<Self::ComposableLimitOrder> {
-        Box::pin(futures_util::future::join_all(
-            transactions
-                .into_iter()
-                .map(|(origin, tx)| self.validate_composable_order(origin, tx))
-        ))
-    }
-
-    fn validate_searcher_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::SearcherOrder
-    ) -> ValidationFuture<Self::SearcherOrder>;
-
-    fn validate_searcher_orders(
-        &self,
-        transactions: Vec<(OrderOrigin, Self::SearcherOrder)>
-    ) -> ValidationsFuture<Self::SearcherOrder> {
-        Box::pin(futures_util::future::join_all(
-            transactions
-                .into_iter()
-                .map(|(origin, tx)| self.validate_searcher_order(origin, tx))
-        ))
-    }
-
-    fn validate_composable_searcher_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::ComposableSearcherOrder
-    ) -> ValidationFuture<Self::ComposableSearcherOrder>;
-
-    fn validate_composable_searcher_orders(
-        &self,
-        transactions: Vec<(OrderOrigin, Self::ComposableSearcherOrder)>
-    ) -> ValidationsFuture<Self::ComposableSearcherOrder> {
-        Box::pin(futures_util::future::join_all(
-            transactions
-                .into_iter()
-                .map(|(origin, tx)| self.validate_composable_searcher_order(origin, tx))
-        ))
-    }
 }
 
 impl OrderValidatorHandle for ValidationClient {
-    /// The transaction type of the composable limit order pool
-    type ComposableLimitOrder = EcRecoveredComposableLimitOrder;
-    /// The transaction type of the composable searcher pool
-    type ComposableSearcherOrder = EcRecoveredComposableSearcherOrder;
-    /// The order type of the limit order pool
-    type LimitOrder = EcRecoveredLimitOrder;
-    /// The transaction type of the searcher order pool
-    type SearcherOrder = EcRecoveredSearcherOrder;
+    type Order = AllOrders;
 
     fn validate_order(
         &self,
         origin: OrderOrigin,
-        transaction: Self::LimitOrder
-    ) -> ValidationFuture<Self::LimitOrder> {
+        transaction: Self::Order
+    ) -> ValidationFuture<Self::Order> {
         Box::pin(async move {
             let (tx, rx) = channel();
-            let _ = self
-                .0
-                .send(ValidationRequest::Order(OrderValidationRequest::ValidateLimit(
-                    tx,
-                    origin,
-                    transaction
-                )));
-
-            rx.await.unwrap()
-        })
-    }
-
-    fn validate_composable_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::ComposableLimitOrder
-    ) -> ValidationFuture<Self::ComposableLimitOrder> {
-        Box::pin(async move {
-            let (tx, rx) = channel();
-            let _ = self.0.send(ValidationRequest::Order(
-                OrderValidationRequest::ValidateComposableLimit(tx, origin, transaction)
-            ));
-
-            rx.await.unwrap()
-        })
-    }
-
-    fn validate_searcher_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::SearcherOrder
-    ) -> ValidationFuture<Self::SearcherOrder> {
-        Box::pin(async move {
-            let (tx, rx) = channel();
-            let _ =
-                self.0
-                    .send(ValidationRequest::Order(OrderValidationRequest::ValidateSearcher(
-                        tx,
-                        origin,
-                        transaction
-                    )));
-
-            rx.await.unwrap()
-        })
-    }
-
-    fn validate_composable_searcher_order(
-        &self,
-        origin: OrderOrigin,
-        transaction: Self::ComposableSearcherOrder
-    ) -> ValidationFuture<Self::ComposableSearcherOrder> {
-        Box::pin(async move {
-            let (tx, rx) = channel();
-            let _ = self.0.send(ValidationRequest::Order(
-                OrderValidationRequest::ValidateComposableSearcher(tx, origin, transaction)
-            ));
+            // let _ = self
+            //     .0
+            //     .send(ValidationRequest::Order(OrderValidationRequest::ValidateLimit(
+            //         tx,
+            //         origin,
+            //         transaction
+            //     )));
 
             rx.await.unwrap()
         })

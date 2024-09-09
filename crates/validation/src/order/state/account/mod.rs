6448bc3297526c4aa2e85204eea1d989f635833a
Will Smith <williamqsmith11@gmail.com>
2024-09-09 17:25:55 -0500

    wip

--- crates/validation/src/order/state/account/mod.rs
+++ crates/validation/src/order/state/account/mod.rs
@@ -220,8 +220,64 @@ pub mod tests {
         processor
             .verify_order(order, pool_info, 420, true)
             .expect("order should be valid");
-        // assert!(matches!(e,
-        // UserAccountVerificationError::DuplicateNonce(..)));
+    }
+
+    #[test]
+    fn test_failure_on_duplicate_pending_nonce() {
+        let block = 420;
+        let mut processor = setup_test_account_processor(block);
+
+        let user = Address::random();
+        let asset0 = 0;
+        let asset1 = 1;
+
+        let token0 = Address::random();
+        let token1 = Address::random();
+        let pool = 10;
+
+        let mut mock_pool = MockPoolTracker::default();
+
+        mock_pool.add_pool(token0, token1, pool);
+        mock_pool.add_asset(asset0, token0);
+        mock_pool.add_asset(asset1, token1);
+
+        let mut rng = thread_rng();
+        let mut order: GroupedVanillaOrder = generate_limit_order(
+            &mut rng,
+            false,
+            true,
+            Some(pool as usize),
+            None,
+            Some(asset0),
+            Some(asset1),
+            Some(420)
+        )
+        .order;
+
+        // wrap order with details
+        let (pool_info, order) = mock_pool
+            .fetch_pool_info_for_order(order)
+            .expect("pool tracker should have valid state");
+
+        processor.fetch_utils.set_balance_for_user(
+            user,
+            token0,
+            U256::from(order.amount_in()) * U256::from(2)
+        );
+        processor.fetch_utils.set_approval_for_user(
+            user,
+            token0,
+            U256::from(order.amount_in()) * U256::from(2)
+        );
+
+        // first time verifying should pass
+        processor
+            .verify_order(order.clone(), pool_info.clone(), 420, true)
+            .expect("order should be valid");
+
+        // second time should fail
+        let Err(e) = processor.verify_order(order, pool_info, 420, true) else { panic!("verifying order should of failed")};
+        assert!(matches!(e, UserAccountVerificationError::DuplicateNonce(..)));
     }
 
     #[test]

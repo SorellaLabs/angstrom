63da2e90fd39588fdf36f9ecea6ea86fc7f512af
Will Smith <williamqsmith11@gmail.com>
2024-09-09 11:57:59 -0500

    fix

--- crates/angstrom-net/src/types/message.rs
+++ crates/angstrom-net/src/types/message.rs
@@ -74,8 +74,9 @@ pub struct StromProtocolMessage {
 impl StromProtocolMessage {
     pub fn decode_message(buf: &mut &[u8]) -> Result<Self, StromStreamError> {
         let message_id: StromMessageID = Decodable::decode(buf)?;
+        let data: Vec<u8> = Decodable::decode(buf)?;
         let mut config = standard().with_no_limit().with_big_endian();
-        let message: StromMessage = decode_from_slice(buf, config).map(|f| f.0).unwrap();
+        let message: StromMessage = decode_from_slice(&data, config).map(|f| f.0).unwrap();
 
         Ok(StromProtocolMessage { message_id, message })
     }

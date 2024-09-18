064db62c43770e5457bd0ee5e6ac9fb1c49a548e
Plamen Hristov <phristov@blocksoft.ltd>
2024-09-18 17:25:06 +0300

    AG-161: RPC and subscription (#203)
    * Order subscription and order validation
    
    * Removed unused function
    
    * Removed unused deps
    
    * Added explicit handling of order types and subscriptions
    
    * Addressed comments and fixed some things.

--- crates/order-pool/src/lib.rs
+++ crates/order-pool/src/lib.rs
@@ -8,6 +8,8 @@ pub mod order_storage;
 mod searcher;
 mod validator;
 
+use std::future::Future;
+
 use angstrom_types::{orders::OrderOrigin, sol_bindings::grouped_orders::AllOrders};
 pub use angstrom_utils::*;
 pub use config::PoolConfig;
@@ -17,13 +19,18 @@ use tokio::sync::broadcast::Receiver;
 #[derive(Debug, Clone)]
 pub enum PoolManagerUpdate {
     NewOrder(AllOrders),
-    FilledOrder(AllOrders)
+    FilledOrder((u64, AllOrders)),
+    UnfilledOrders(AllOrders)
 }
 
 /// The OrderPool Trait is how other processes can interact with the orderpool
 /// asyncly. This allows for requesting data and providing data from different
 /// threads efficiently.
 pub trait OrderPoolHandle: Send + Sync + Clone + Unpin + 'static {
-    fn new_order(&self, origin: OrderOrigin, order: AllOrders) -> bool;
+    fn new_order(
+        &self,
+        order_origin: OrderOrigin,
+        order: AllOrders
+    ) -> impl Future<Output = bool> + Send;
     fn subscribe_orders(&self) -> Receiver<PoolManagerUpdate>;
 }

faeb825096350c6977e8a2bf80fff043023573b6
Will Smith <williamqsmith11@gmail.com>
2024-10-31 18:11:56 -0400

    wip

--- crates/validation/src/order/state/db_state_utils/finders.rs
+++ crates/validation/src/order/state/db_state_utils/finders.rs
@@ -1,21 +1,66 @@
 // Given that the gas spec when fuzzing balances runs off the assumption
 // that all tokens are ERC-20 which all are written in solidity and don't
 // pack the balance / approval slots. we align to the same assumptions here.
-use std::sync::Arc;
+use std::{fmt::Debug, sync::Arc};
 
-use alloy::primitives::Address;
+use alloy::{
+    primitives::{keccak256, Address, U256},
+    sol_types::{SolCall, SolStruct, SolValue}
+};
+use revm::{
+    db::CacheDB,
+    primitives::{EnvWithHandlerCfg, ResultAndState, TxEnv, TxKind},
+    DatabaseRef
+};
 
 alloy::sol!(
-function transfer(address _to, uint256 _value) public returns (bool success);
-function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-function approve(address _spender, uint256 _value) public returns (bool success);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 );
 
 /// panics if we cannot find the slot for the given token
-fn find_slot_offset_for_balance<DB: revm::DatabaseRef>(db: Arc<DB>) -> u64 {
+fn find_slot_offset_for_balance<DB: revm::DatabaseRef>(db: Arc<DB>, token_address: Address) -> u64
+where
+    <DB as revm::DatabaseRef>::Error: Debug
+{
     let probe_address = Address::random();
 
-    todo!()
+    let mut db = CacheDB::new(db.clone());
+    let evm_handler = EnvWithHandlerCfg::default();
+    let mut evm = revm::Evm::builder()
+        .with_ref_db(db.clone())
+        .with_env_with_handler_cfg(evm_handler)
+        .modify_env(|env| {
+            env.cfg.disable_balance_check = true;
+        })
+        .build();
+
+    // check the first 100 offsets
+    for offset in 0..100 {
+        // set balance
+        let balance_slot = keccak256((probe_address, offset).abi_encode());
+        db.insert_account_storage(token_address, balance_slot.into(), U256::from(123456789))
+            .unwrap();
+        // execute revm to see if we hit the slot
+        evm = evm
+            .modify()
+            .modify_tx_env(|tx| {
+                tx.caller = probe_address;
+                tx.transact_to = TxKind::Call(token_address);
+                tx.data = balanceOfCall::new((probe_address,)).abi_encode().into();
+                tx.value = U256::from(0);
+                tx.nonce = None;
+            })
+            .build();
+
+        let output = evm.transact().unwrap().result.output().unwrap();
+        let output_balance = balanceOfReturn::abi_decode(output, false);
+        if output_balance.balance == U256::from(123456789) {
+            return offset as u64
+        }
+    }
+
+    panic!("was not able to find balance offset");
 }
 
 /// panics if we cannot prove the slot for the given token

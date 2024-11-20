ad739d48f6f83757591daca714313059b064d88b
Plamen Hristov <phristov@blocksoft.ltd>
2024-11-21 00:50:11 +0200

    Removing unnecessary function

--- crates/matching-engine/src/lib.rs
+++ crates/matching-engine/src/lib.rs
@@ -1,29 +1,16 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc
-};
+use std::collections::{HashMap, HashSet};
 
-use alloy::{
-    providers::{Network, Provider},
-    transports::Transport
-};
-use alloy_primitives::{Address, BlockNumber};
+use alloy_primitives::Address;
 use angstrom_types::{
-    block_sync::BlockSyncConsumer,
     consensus::PreProposal,
     contract_payloads::angstrom::BundleGasDetails,
     matching::uniswap::PoolSnapshot,
     orders::PoolSolution,
-    primitive::{PoolId, UniswapPoolRegistry},
+    primitive::PoolId,
     sol_bindings::grouped_orders::{GroupedVanillaOrder, OrderWithStorageData}
 };
 use book::OrderBook;
 use futures_util::future::BoxFuture;
-use reth_provider::CanonStateNotifications;
-use uniswap_v4::uniswap::{
-    pool::EnhancedUniswapPool, pool_data_loader::DataLoader, pool_manager::UniswapPoolManager,
-    pool_providers::canonical_state_adapter::CanonicalStateAdapter
-};
 
 pub mod book;
 pub mod manager;
@@ -49,48 +36,4 @@ pub fn build_book(
     let (bids, asks) = orders.into_iter().partition(|o| o.is_bid);
 
     OrderBook::new(id, amm, bids, asks, Some(book::sort::SortStrategy::ByPriceByVolume))
-}
-
-pub async fn configure_uniswap_manager<
-    T: Transport + Clone,
-    N: Network,
-    BlockSync: BlockSyncConsumer
->(
-    provider: Arc<impl Provider<T, N>>,
-    state_notification: CanonStateNotifications,
-    uniswap_pool_registry: UniswapPoolRegistry,
-    current_block: BlockNumber,
-    block_sync: BlockSync,
-    pool_manager: Address
-) -> UniswapPoolManager<CanonicalStateAdapter, BlockSync, DataLoader<PoolId>, PoolId> {
-    let mut uniswap_pools: Vec<_> = uniswap_pool_registry
-        .pools()
-        .keys()
-        .map(|pool_id| {
-            let initial_ticks_per_side = 200;
-            EnhancedUniswapPool::new(
-                DataLoader::new_with_registry(
-                    *pool_id,
-                    uniswap_pool_registry.clone(),
-                    pool_manager
-                ),
-                initial_ticks_per_side
-            )
-        })
-        .collect();
-
-    for pool in uniswap_pools.iter_mut() {
-        pool.initialize(Some(current_block), provider.clone())
-            .await
-            .unwrap();
-    }
-
-    let state_change_buffer = 100;
-    UniswapPoolManager::new(
-        uniswap_pools,
-        current_block,
-        state_change_buffer,
-        Arc::new(CanonicalStateAdapter::new(state_notification)),
-        block_sync
-    )
 }

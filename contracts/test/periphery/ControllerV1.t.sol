353314e1157d03b6da44d08729b7d901e7d5f00d
philogy <philippe@dumo.net>
2024-11-22 12:20:02 +0700

    ♻️ Convert ControllerV1 to solidity

--- contracts/test/periphery/ControllerV1.t.sol
+++ contracts/test/periphery/ControllerV1.t.sol
@@ -4,20 +4,23 @@ pragma solidity ^0.8.0;
 import {BaseTest} from "test/_helpers/BaseTest.sol";
 import {Angstrom} from "src/Angstrom.sol";
 import {PoolManager} from "v4-core/src/PoolManager.sol";
-import {IControllerV1} from "test/_helpers/IControllerV1.sol";
+import {ControllerV1} from "src/periphery/ControllerV1.sol";
 import {TopLevelAuth} from "src/modules/TopLevelAuth.sol";
 import {LibSort} from "solady/src/utils/LibSort.sol";
 import {
     PoolConfigStoreLib, PoolConfigStore, StoreKey
 } from "../../src/libraries/PoolConfigStore.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 import {console} from "forge-std/console.sol";
 
 /// @author philogy <https://github.com/philogy>
 contract ControllerV1Test is BaseTest {
+    uint256 constant SCHEDULE_TO_CONFIRM_DELAY = 2 weeks;
+
     Angstrom angstrom;
     PoolManager uni;
-    IControllerV1 controller;
+    ControllerV1 controller;
 
     address pm_owner = makeAddr("pm_owner");
     address temp_controller = makeAddr("temp_controller");
@@ -26,15 +29,14 @@ contract ControllerV1Test is BaseTest {
     function setUp() public {
         uni = new PoolManager(pm_owner);
         angstrom = Angstrom(deployAngstrom(type(Angstrom).creationCode, uni, temp_controller));
-        controller =
-            IControllerV1(deployCode("ControllerV1", abi.encode(angstrom, controller_owner)));
+        controller = new ControllerV1(angstrom, controller_owner);
         vm.prank(temp_controller);
         angstrom.setController(address(controller));
     }
 
     function test_fuzzing_initializesOwner(address startingOwner) public {
-        IControllerV1 c =
-            IControllerV1(deployCode("ControllerV1", abi.encode(angstrom, startingOwner)));
+        vm.assume(startingOwner != address(0));
+        ControllerV1 c = new ControllerV1(angstrom, startingOwner);
         assertEq(c.owner(), startingOwner);
     }
 
@@ -42,37 +44,42 @@ contract ControllerV1Test is BaseTest {
         vm.assume(newOwner != address(0));
 
         vm.prank(controller_owner);
-        controller.transfer_ownership(newOwner);
+        controller.transferOwnership(newOwner);
         vm.prank(newOwner);
-        controller.accept_ownership();
+        controller.acceptOwnership();
 
         assertEq(controller.owner(), newOwner);
 
         if (newOwner != controller_owner) {
             vm.prank(controller_owner);
-            vm.expectRevert("ownable: caller is not the owner");
-            controller.transfer_ownership(controller_owner);
+            vm.expectRevert(
+                abi.encodeWithSelector(
+                    Ownable.OwnableUnauthorizedAccount.selector, controller_owner
+                )
+            );
+            controller.transferOwnership(controller_owner);
         }
     }
 
     function test_can_cancelNewController() public {
         address bad_controller = makeAddr("bad_controller");
-        uint256 unlockTime = block.timestamp + controller.SCHEDULE_TO_CONFIRM_DELAY();
+        uint256 unlockTime = block.timestamp + SCHEDULE_TO_CONFIRM_DELAY;
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.NewControllerScheduled(bad_controller, unlockTime);
+        emit ControllerV1.NewControllerScheduled(bad_controller);
         vm.prank(controller_owner);
-        controller.schedule_new_controller(bad_controller);
+        controller.scheduleNewController(bad_controller);
 
         skip(1 days);
 
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.NewControllerCancelled();
+        emit ControllerV1.NewControllerCancelled();
         vm.prank(controller_owner);
-        controller.cancel_pending_controller();
+        controller.cancelPendingController();
 
         vm.warp(unlockTime);
-        vm.expectRevert("Controller not pending");
-        controller.confirm_pending_controller();
+        vm.expectRevert(ControllerV1.ControllerNotPending.selector);
+        vm.prank(controller_owner);
+        controller.cancelPendingController();
     }
 
     function test_configurePools() public {
@@ -81,9 +88,9 @@ contract ControllerV1Test is BaseTest {
         LibSort.sort(assets);
 
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.PoolConfigured(assets[0], assets[2], 100, 0);
+        emit ControllerV1.PoolConfigured(assets[0], assets[2], 100, 0);
         vm.prank(controller_owner);
-        controller.configure_pool(assets[0], assets[2], 100, 0);
+        controller.configurePool(assets[0], assets[2], 100, 0);
 
         PoolConfigStore store = PoolConfigStore.wrap(rawGetConfigStore(address(angstrom)));
         assertEq(store.totalEntries(), 1);
@@ -91,43 +98,45 @@ contract ControllerV1Test is BaseTest {
         (int24 tickSpacing, uint24 feeInE6) = store.get(key, 0);
         assertEq(tickSpacing, 100);
         assertEq(feeInE6, 0);
-        (address asset0, address asset1) = controller.pools(StoreKey.unwrap(key));
+        (address asset0, address asset1) = controller.pools(key);
         assertEq(asset0, assets[0]);
         assertEq(asset1, assets[2]);
 
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.PoolRemoved(assets[0], assets[2]);
+        emit ControllerV1.PoolRemoved(assets[0], assets[2]);
         vm.prank(controller_owner);
-        controller.remove_pool(assets[0], assets[2]);
+        controller.removePool(assets[0], assets[2]);
     }
 
     function test_fuzzing_preventsNonOwnerTransfer(address nonOwner, address newOwner) public {
         vm.assume(nonOwner != controller_owner);
         vm.prank(nonOwner);
-        vm.expectRevert("ownable: caller is not the owner");
-        controller.transfer_ownership(newOwner);
+        vm.expectRevert(
+            abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, nonOwner)
+        );
+        controller.transferOwnership(newOwner);
     }
 
     function test_controllerMigration() public {
         address next_controller = makeAddr("next_controller");
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.NewControllerScheduled(
-            next_controller, block.timestamp + controller.SCHEDULE_TO_CONFIRM_DELAY()
-        );
+        emit ControllerV1.NewControllerScheduled(next_controller);
         vm.prank(controller_owner);
-        controller.schedule_new_controller(next_controller);
-        assertEq(controller.pending_controller(), next_controller);
+        controller.scheduleNewController(next_controller);
+        assertEq(controller.pendingController(), next_controller);
 
         skip(2 days);
 
-        vm.expectRevert("New controller still pending");
-        controller.confirm_pending_controller();
+        vm.expectRevert(ControllerV1.ControllerStillPending.selector);
+        vm.prank(next_controller);
+        controller.acceptNewController();
 
-        skip(controller.SCHEDULE_TO_CONFIRM_DELAY());
+        skip(SCHEDULE_TO_CONFIRM_DELAY);
 
         vm.expectEmit(true, true, true, true);
-        emit IControllerV1.NewControllerConfirmed(next_controller);
-        controller.confirm_pending_controller();
+        emit ControllerV1.NewControllerAccepted();
+        vm.prank(next_controller);
+        controller.acceptNewController();
 
         assertEq(rawGetController(address(angstrom)), next_controller);
     }
@@ -142,19 +151,19 @@ contract ControllerV1Test is BaseTest {
             makeAddr("addr_4"),
             makeAddr("addr_5")
         ];
-        assertEq(controller.total_nodes(), 0);
+        assertEq(controller.totalNodes(), 0);
         for (uint256 i = 0; i < addrs.length; i++) {
             vm.expectEmit(true, true, true, true);
-            emit IControllerV1.NodeAdded(addrs[i]);
+            emit ControllerV1.NodeAdded(addrs[i]);
             vm.prank(controller_owner);
-            controller.add_node(addrs[i]);
+            controller.addNode(addrs[i]);
             assertTrue(_isNode(addrs[i]), "expected to be node after add");
-            assertEq(controller.total_nodes(), i + 1);
+            assertEq(controller.totalNodes(), i + 1);
             for (uint256 j = 0; j < i; j++) {
-                uint256 totalNodes = controller.total_nodes();
+                uint256 totalNodes = controller.totalNodes();
                 bool found = false;
                 for (uint256 k = 0; k < totalNodes; k++) {
-                    if (controller.nodes(k) == addrs[j]) {
+                    if (controller.nodes()[k] == addrs[j]) {
                         found = true;
                         break;
                     }
@@ -169,17 +178,17 @@ contract ControllerV1Test is BaseTest {
             uint256 ai = removeMap[i];
 
             vm.expectEmit(true, true, true, true);
-            emit IControllerV1.NodeRemoved(addrs[ai]);
+            emit ControllerV1.NodeRemoved(addrs[ai]);
             vm.prank(controller_owner);
-            controller.remove_node(addrs[ai]);
+            controller.removeNode(addrs[ai]);
             removed[ai] = true;
-            assertEq(controller.total_nodes(), _TOTAL_NODES - i - 1);
+            assertEq(controller.totalNodes(), _TOTAL_NODES - i - 1);
 
             for (uint256 j = 0; j < addrs.length; j++) {
-                uint256 totalNodes = controller.total_nodes();
+                uint256 totalNodes = controller.totalNodes();
                 bool found = false;
                 for (uint256 k = 0; k < totalNodes; k++) {
-                    if (controller.nodes(k) == addrs[j]) {
+                    if (controller.nodes()[k] == addrs[j]) {
                         found = true;
                         break;
                     }
@@ -204,14 +213,5 @@ contract ControllerV1Test is BaseTest {
             require(keccak256(error) == keccak256(abi.encodePacked(TopLevelAuth.NotNode.selector)));
             return false;
         }
-    }
-
-    function _allNodes() internal view returns (string memory) {
-        if (controller.total_nodes() == 0) return "[]";
-
-        string memory s = string.concat("[", vm.toString(controller.nodes(0))); // ]
-        for (uint256 i = 1; i < controller.total_nodes(); i++) {}
-
-        return string.concat(s, "]");
     }
 }

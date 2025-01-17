62dc4d597b4d7a67b33df9d3521310a8dbd87f7e
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-17 13:57:32 -0500

    wip

--- testing-tools/src/contracts/environment/angstrom.rs
+++ testing-tools/src/contracts/environment/angstrom.rs
@@ -42,7 +42,7 @@ where
     async fn deploy_angstrom(inner: &E, nodes: Vec<Address>) -> eyre::Result<Address> {
         let provider = inner.provider();
         let key = provider.default_signer_address();
-        debug!(?key, "Deploying Angstrom...");
+        debug!("Deploying Angstrom...");
         let angstrom_addr = inner
             .execute_then_mine(deploy_angstrom(
                 provider,

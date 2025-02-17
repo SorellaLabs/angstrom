d999d11852df8dd1da626a4b7f8ff5e177e322f3
0xNONSO <0xnonso@gmail.com>
2025-02-17 23:21:36 +0100

    cleanup

--- contracts/src/modules/OrderInvalidation.sol
+++ contracts/src/modules/OrderInvalidation.sol
@@ -16,16 +16,18 @@ abstract contract OrderInvalidation {
     /// @dev `keccak256("angstrom-v1_0.twap-unordered-nonces.slot")[0:4]`
     uint256 private constant UNORDERED_TWAP_NONCES_SLOT = 0x635a0808;
     // type(uint32).max
-    uint256 private constant MASK_U32 = 4294967295;
+    uint256 private constant MASK_U32 = 0xffffffff;
     // type(uint40).max
-    uint256 private constant MASK_U40 = 1099511627775;
+    uint256 private constant MASK_U40 = 0xffffffffff;
     // type(uint64).max
-    uint256 private constant MASK_U64 = 18446744073709551615;
+    uint256 private constant MASK_U64 = 0xffffffffffffffff;
     // type(uint232).max
-    uint256 private constant MASK_U232 = 6901746346790563787434755862277025452451108972170386555162524223799295;
-    // max upper limit of twap intervals = once very 365.25 days
+    uint256 private constant MASK_U232 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    // max twap nonce bit
+    uint256 private constant MAX_TWAP_NONCE_SIZE = 232;
+    // max upper limit of twap intervals = 365.25 days
     uint256 private constant MAX_TWAP_INTERVAL = 0x1e187e0;
-    // max no. of order parts = 365.25 days / 5 seconds
+    // max no. of order parts = 6311520 (365.25 days / 5 seconds)
     uint256 private constant MAX_TWAP_TOTAL_PARTS = 0x604e60;
 
     function invalidateNonce(uint64 nonce) external {
@@ -35,36 +37,36 @@ abstract contract OrderInvalidation {
     function invalidateTWAPNonce(uint64 nonce) external {
         assembly ("memory-safe") {
             nonce := and(nonce, MASK_U64)
-            mstore(12, div(nonce, 232))
+            mstore(12, div(nonce, MAX_TWAP_NONCE_SIZE))
             mstore(4, UNORDERED_TWAP_NONCES_SLOT)
             mstore(0, caller())
 
             let bitmapPtr := keccak256(12, 32)
-            let flag := shl(mod(nonce, 232), 1)
+            let flag := shl(mod(nonce, MAX_TWAP_NONCE_SIZE), 1)
             let bitmapVal := sload(bitmapPtr)
             let updated := xor(and(bitmapVal, MASK_U232) , flag)
             let twapNonce := iszero(and(updated, flag))
-            let fParts := shr(232, bitmapVal)
+            let fulfilledParts := shr(MAX_TWAP_NONCE_SIZE, bitmapVal)
 
-            if xor(iszero(iszero(fParts)), twapNonce) {
+            if xor(iszero(iszero(fulfilledParts)), twapNonce) {
                 mstore(0x00, 0xcfa42043 /* InvalidTWAPNonce() */ )
                 revert(0x1c, 0x04)
             }
 
-            if eq(fParts, 0xffffff) {
+            if eq(fulfilledParts, 0xffffff) {
                 mstore(0x00, 0x9a495418 /* TWAPNonceReuse() */ )
                 revert(0x1c, 0x04)
             }
 
-            sstore(bitmapPtr, or(updated, shl(232, 0xffffff)))
+            sstore(bitmapPtr, or(updated, shl(MAX_TWAP_NONCE_SIZE, 0xffffff)))
         }
     }
 
-    function _checkTWAPOrderData(uint32 interval, uint32 tParts) internal pure {
-        bool validInterval = interval != 0 && interval < MAX_TWAP_INTERVAL;
-        bool validTParts = tParts != 0 && tParts < MAX_TWAP_TOTAL_PARTS;
+    function _checkTWAPOrderData(uint32 interval, uint32 twapParts) internal pure {
+        bool validInterval = interval != 0 && interval <= MAX_TWAP_INTERVAL;
+        bool validTParts = twapParts != 0 && twapParts <= MAX_TWAP_TOTAL_PARTS;
 
-        assembly {
+        assembly("memory-safe") {
             if iszero(and(validInterval, validTParts)){
                 mstore(0x00, 0x51e490f3 /* InvalidTWAPOrder() */ )
                 revert(0x1c, 0x04)
@@ -96,51 +98,54 @@ abstract contract OrderInvalidation {
         }
     }
 
-    function _invalidatePartTWAPNonceAndCheckDeadline(address owner, uint64 nonce, uint40 sTime, uint32 interval, uint32 tParts) 
+    function _invalidatePartTWAPNonceAndCheckDeadline(
+        address owner, 
+        uint64 nonce, 
+        uint40 startTime, 
+        uint32 interval, 
+        uint32 twapParts
+    ) 
         internal 
     {
-        uint256 _fParts;
         assembly ("memory-safe") {
             nonce := and(nonce, MASK_U64)
-            mstore(12, div(nonce, 232))
+            mstore(12, div(nonce, MAX_TWAP_NONCE_SIZE))
             mstore(4, UNORDERED_TWAP_NONCES_SLOT)
             mstore(0, owner)
 
             let bitmapPtr := keccak256(12, 32)
-            let flag := shl(mod(nonce, 232), 1)
+            let flag := shl(mod(nonce, MAX_TWAP_NONCE_SIZE), 1)
             let bitmapVal := sload(bitmapPtr)
             let updated := xor(and(bitmapVal, MASK_U232), flag)
             let twapNonce := iszero(and(updated, flag))
 
             // part to fulfill
-            let fParts := shr(232, bitmapVal)
-            _fParts := fParts
+            let fulfilledParts := shr(MAX_TWAP_NONCE_SIZE, bitmapVal)
+            let _cachedFulfilledParts := fulfilledParts
 
-            if xor(iszero(iszero(fParts)), twapNonce) {
+            if xor(iszero(iszero(fulfilledParts)), twapNonce) {
                 mstore(0x00, 0xcfa42043 /* InvalidTWAPNonce() */ )
                 revert(0x1c, 0x04)
             }
             
-            fParts := add(fParts, 1)
-            tParts:= and(tParts, MASK_U32)
+            fulfilledParts := add(fulfilledParts, 1)
+            twapParts:= and(twapParts, MASK_U32)
 
-            if gt(fParts, tParts) {
+            if gt(fulfilledParts, twapParts) {
                 mstore(0x00, 0x9a495418 /* TWAPNonceReuse() */ )
                 revert(0x1c, 0x04)
             }
 
-            updated := or(shl(232, fParts), flag)
+            updated := or(shl(MAX_TWAP_NONCE_SIZE, fulfilledParts), flag)
 
-            if iszero(sub(tParts, fParts)) {
-                updated := or(updated, shl(232, 0xffffff))
+            if iszero(sub(twapParts, fulfilledParts)) {
+                updated := or(updated, shl(MAX_TWAP_NONCE_SIZE, 0xffffff))
             }
             sstore(bitmapPtr, updated)
-        }
 
-        assembly ("memory-safe") {
-            let cPartStart := add(and(sTime, MASK_U40), mul(_fParts, and(interval, MASK_U32)))
+            let currentPartStart := add(and(startTime, MASK_U40), mul(_cachedFulfilledParts, and(interval, MASK_U32)))
             
-            if or(lt(timestamp(), cPartStart), gt(timestamp(), add(cPartStart, interval))) {
+            if or(lt(timestamp(), currentPartStart), gt(timestamp(), add(currentPartStart, interval))) {
                 mstore(0x00, 0x982c606d /* TWAPExpired() */ )
                 revert(0x1c, 0x04)
             }

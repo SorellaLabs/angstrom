458fa0846be0c3ea5e6d843f363dece2fdaf31a9
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-07-25 13:44:06 +0200

    feat(op-angstrom): extract shared methods to generic amm-quoter

--- crates/amm-quoter/src/consensus.rs
+++ crates/amm-quoter/src/consensus.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet, hash_map::Entry},
+    collections::{HashMap, HashSet},
     pin::Pin,
     sync::Arc,
     task::Poll,
@@ -13,17 +13,13 @@ use angstrom_types::{
     primitive::PoolId,
     sol_bindings::{grouped_orders::AllOrders, rpc_orders::TopOfBlockOrder}
 };
-use futures::{FutureExt, Stream, StreamExt, TryFutureExt, stream::FuturesUnordered};
-use matching_engine::strategy::BinarySearchStrategy;
+use futures::{Stream, StreamExt, stream::FuturesUnordered};
 use order_pool::order_storage::OrderStorage;
 use rayon::ThreadPool;
-use tokio::{
-    sync::{mpsc, oneshot},
-    time::interval
-};
+use tokio::{sync::mpsc, time::interval};
 use uniswap_v4::uniswap::{pool_data_loader::PoolDataLoader, pool_manager::SyncedUniswapPools};
 
-use crate::{QuoterManager, Slot0Update, build_non_proposal_books};
+use crate::{QuoterManager, Slot0Update};
 
 /// Mode for consensus-based order book building.
 pub struct ConsensusMode {
@@ -83,30 +79,7 @@ impl<BlockSync: BlockSyncConsumer> QuoterManager<BlockSync, ConsensusMode> {
         }
     }
 
-    fn handle_new_subscription(&mut self, pools: HashSet<PoolId>, chan: mpsc::Sender<Slot0Update>) {
-        let keys = self
-            .book_snapshots
-            .iter()
-            .flat_map(|(ang_key, (uni_key, _))| [ang_key, uni_key])
-            .copied()
-            .collect::<HashSet<_>>();
-
-        for pool in &pools {
-            if !keys.contains(pool) {
-                // invalid subscription
-                return;
-            }
-        }
-
-        for pool in pools {
-            self.pool_to_subscribers
-                .entry(pool)
-                .or_default()
-                .push(chan.clone());
-        }
-    }
-
-    fn all_orders_with_consensus(&self) -> OrderSet<AllOrders, TopOfBlockOrder> {
+    pub(crate) fn all_orders_with_consensus(&self) -> OrderSet<AllOrders, TopOfBlockOrder> {
         if let Some(hashes) = self.mode.active_pre_proposal_aggr_order_hashes.as_ref() {
             self.orders
                 .get_all_orders_with_hashes(&hashes.limit, &hashes.searcher)
@@ -115,99 +88,10 @@ impl<BlockSync: BlockSyncConsumer> QuoterManager<BlockSync, ConsensusMode> {
         }
     }
 
-    fn spawn_book_solvers(&mut self, seq_id: u16) {
-        let OrderSet { limit, searcher } = self.all_orders_with_consensus();
-        let mut books = build_non_proposal_books(limit, &self.book_snapshots);
-
-        let searcher_orders = searcher
-            .into_iter()
-            .fold(HashMap::new(), |mut acc, searcher| {
-                match acc.entry(searcher.pool_id) {
-                    Entry::Vacant(v) => {
-                        v.insert(searcher);
-                    }
-                    Entry::Occupied(mut o) => {
-                        let current = o.get();
-                        // if this order on same pool_id has a higher tob reward or they are the
-                        // same and it has a lower order hash. replace
-                        if searcher.tob_reward > current.tob_reward
-                            || (searcher.tob_reward == current.tob_reward
-                                && searcher.order_id.hash < current.order_id.hash)
-                        {
-                            o.insert(searcher);
-                        }
-                    }
-                };
-
-                acc
-            });
-
-        for (book_id, (uni_pool_id, amm)) in &self.book_snapshots {
-            // Default as if we don't have a book, we want to still send update.
-            let mut book = books.remove(book_id).unwrap_or_default();
-            book.id = *book_id;
-            book.set_amm_if_missing(|| amm.clone());
-
-            let searcher = searcher_orders.get(&book.id()).cloned();
-            let (tx, rx) = oneshot::channel();
-            let block = self.cur_block;
-
-            let uni_pool_id = *uni_pool_id;
-
-            self.threadpool.spawn(move || {
-                let b = book;
-                let (sqrt_price, tick, liquidity) =
-                    BinarySearchStrategy::give_end_amm_state(&b, searcher);
-                let update = Slot0Update {
-                    current_block: block,
-                    seq_id,
-                    angstrom_pool_id: b.id(),
-                    uni_pool_id,
-                    liquidity,
-                    sqrt_price_x96: sqrt_price,
-                    tick
-                };
-
-                tx.send(update).unwrap()
-            });
-
-            self.pending_tasks.push(rx.map_err(Into::into).boxed())
-        }
-    }
-
-    fn update_book_state(&mut self) {
-        self.book_snapshots = self
-            .amms
-            .iter()
-            .map(|entry| {
-                let pool_lock = entry.value().read().unwrap();
-                let uni_key = pool_lock.data_loader().private_address();
-                let snapshot_data = pool_lock.fetch_pool_snapshot().unwrap().2;
-                (*entry.key(), (uni_key, snapshot_data))
-            })
-            .collect();
-    }
-
     fn update_consensus_state(&mut self, round: ConsensusRoundOrderHashes) {
         if matches!(round.round, ConsensusRoundEvent::PropagatePreProposalAgg) {
             self.mode.active_pre_proposal_aggr_order_hashes = Some(round)
         }
-    }
-
-    fn send_out_result(&mut self, slot_update: Slot0Update) {
-        if let Some(ang_pool_subs) = self
-            .pool_to_subscribers
-            .get_mut(&slot_update.angstrom_pool_id)
-        {
-            ang_pool_subs.retain(|subscriber| subscriber.try_send(slot_update.clone()).is_ok());
-        };
-
-        if let Some(uni_pool_subs) = self
-            .pool_to_subscribers
-            .get_mut(&slot_update.angstrom_pool_id)
-        {
-            uni_pool_subs.retain(|subscriber| subscriber.try_send(slot_update.clone()).is_ok());
-        };
     }
 }
 
@@ -257,7 +141,8 @@ impl<BlockSync: BlockSyncConsumer> Future for QuoterManager<BlockSync, Consensus
             // as 65535 is more than enough
             self.seq_id += 1;
 
-            self.spawn_book_solvers(seq_id);
+            let orders = self.all_orders_with_consensus();
+            self.spawn_book_solvers(seq_id, orders);
         }
 
         Poll::Pending
--- crates/amm-quoter/src/lib.rs
+++ crates/amm-quoter/src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, hash_map::Entry},
     fmt::Debug,
     pin::Pin,
     sync::Arc
@@ -7,19 +7,29 @@ use std::{
 
 use alloy::primitives::U160;
 use angstrom_types::{
-    block_sync::BlockSyncConsumer, primitive::PoolId, uni_structure::BaselinePoolState
+    block_sync::BlockSyncConsumer,
+    orders::OrderSet,
+    primitive::PoolId,
+    sol_bindings::{grouped_orders::AllOrders, rpc_orders::TopOfBlockOrder},
+    uni_structure::BaselinePoolState
 };
-use futures::{Stream, StreamExt, future::BoxFuture, stream::FuturesUnordered};
+use futures::{
+    FutureExt, Stream, StreamExt, TryFutureExt, future::BoxFuture, stream::FuturesUnordered
+};
 use matching_engine::{
     book::{BookOrder, OrderBook},
-    build_book
+    build_book,
+    strategy::BinarySearchStrategy
 };
 use order_pool::order_storage::OrderStorage;
 use rayon::ThreadPool;
 use serde::{Deserialize, Serialize};
-use tokio::{sync::mpsc, time::Interval};
+use tokio::{
+    sync::{mpsc, oneshot},
+    time::Interval
+};
 use tokio_stream::wrappers::ReceiverStream;
-use uniswap_v4::uniswap::pool_manager::SyncedUniswapPools;
+use uniswap_v4::uniswap::{pool_data_loader::PoolDataLoader, pool_manager::SyncedUniswapPools};
 
 use crate::consensus::ConsensusMode;
 
@@ -79,6 +89,120 @@ pub struct QuoterManager<BlockSync: BlockSyncConsumer, M = ConsensusMode> {
     execution_interval: Interval,
 
     mode: M
+}
+
+impl<BlockSync: BlockSyncConsumer, M> QuoterManager<BlockSync, M> {
+    fn handle_new_subscription(&mut self, pools: HashSet<PoolId>, chan: mpsc::Sender<Slot0Update>) {
+        let keys = self
+            .book_snapshots
+            .iter()
+            .flat_map(|(ang_key, (uni_key, _))| [ang_key, uni_key])
+            .copied()
+            .collect::<HashSet<_>>();
+
+        for pool in &pools {
+            if !keys.contains(pool) {
+                // invalid subscription
+                return;
+            }
+        }
+
+        for pool in pools {
+            self.pool_to_subscribers
+                .entry(pool)
+                .or_default()
+                .push(chan.clone());
+        }
+    }
+
+    fn spawn_book_solvers(&mut self, seq_id: u16, orders: OrderSet<AllOrders, TopOfBlockOrder>) {
+        let OrderSet { limit, searcher } = orders;
+        let mut books = build_non_proposal_books(limit, &self.book_snapshots);
+
+        let searcher_orders = searcher
+            .into_iter()
+            .fold(HashMap::new(), |mut acc, searcher| {
+                match acc.entry(searcher.pool_id) {
+                    Entry::Vacant(v) => {
+                        v.insert(searcher);
+                    }
+                    Entry::Occupied(mut o) => {
+                        let current = o.get();
+                        // if this order on same pool_id has a higher tob reward or they are the
+                        // same and it has a lower order hash. replace
+                        if searcher.tob_reward > current.tob_reward
+                            || (searcher.tob_reward == current.tob_reward
+                                && searcher.order_id.hash < current.order_id.hash)
+                        {
+                            o.insert(searcher);
+                        }
+                    }
+                };
+
+                acc
+            });
+
+        for (book_id, (uni_pool_id, amm)) in &self.book_snapshots {
+            // Default as if we don't have a book, we want to still send update.
+            let mut book = books.remove(book_id).unwrap_or_default();
+            book.id = *book_id;
+            book.set_amm_if_missing(|| amm.clone());
+
+            let searcher = searcher_orders.get(&book.id()).cloned();
+            let (tx, rx) = oneshot::channel();
+            let block = self.cur_block;
+
+            let uni_pool_id = *uni_pool_id;
+
+            self.threadpool.spawn(move || {
+                let b = book;
+                let (sqrt_price, tick, liquidity) =
+                    BinarySearchStrategy::give_end_amm_state(&b, searcher);
+                let update = Slot0Update {
+                    current_block: block,
+                    seq_id,
+                    angstrom_pool_id: b.id(),
+                    uni_pool_id,
+                    liquidity,
+                    sqrt_price_x96: sqrt_price,
+                    tick
+                };
+
+                tx.send(update).unwrap()
+            });
+
+            self.pending_tasks.push(rx.map_err(Into::into).boxed())
+        }
+    }
+
+    fn update_book_state(&mut self) {
+        self.book_snapshots = self
+            .amms
+            .iter()
+            .map(|entry| {
+                let pool_lock = entry.value().read().unwrap();
+                let uni_key = pool_lock.data_loader().private_address();
+                let snapshot_data = pool_lock.fetch_pool_snapshot().unwrap().2;
+                (*entry.key(), (uni_key, snapshot_data))
+            })
+            .collect();
+    }
+
+    fn send_out_result(&mut self, slot_update: Slot0Update) {
+        if let Some(ang_pool_subs) = self
+            .pool_to_subscribers
+            .get_mut(&slot_update.angstrom_pool_id)
+        {
+            ang_pool_subs.retain(|subscriber| subscriber.try_send(slot_update.clone()).is_ok());
+        };
+
+        if let Some(uni_pool_subs) = self
+            .pool_to_subscribers
+            .get_mut(&slot_update.angstrom_pool_id)
+        {
+            uni_pool_subs.retain(|subscriber| subscriber.try_send(slot_update.clone()).is_ok());
+        };
+    }
 }
 
 pub fn build_non_proposal_books(

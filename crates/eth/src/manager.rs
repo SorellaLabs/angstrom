16616680baa0a1e952df384f656ff73e3e38dd6d
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-11-21 16:03:38 -0500

    tests build + fmt

--- crates/eth/src/manager.rs
+++ crates/eth/src/manager.rs
@@ -391,7 +391,7 @@ pub mod test {
         orders::OrderOutcome,
         primitive::{AngstromAddressConfig, AngstromSigner},
         sol_bindings::grouped_orders::OrderWithStorageData,
-        traits::{BundleProcessing, ChainExt}
+        traits::{ChainExt, UserOrderFromInternal}
     };
     use pade::PadeEncode;
     use reth_ethereum_primitives::Receipt;
